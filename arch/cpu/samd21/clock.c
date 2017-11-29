afef040ed77b9c92922f9aec6a6cb1b9bce9a7d5
Bradley Cage <cage.bradley@gmail.com>
2017-11-28 20:12:04 -0800

    clock seems to maybe compile?

--- arch/cpu/samd21/clock.c
+++ arch/cpu/samd21/clock.c
@@ -27,13 +27,13 @@ clock_init(void)
 /* TBH, this may not be the right config */
 // SysTick_Config(RELOAD_VALUE); /* may work better */
 
-  SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK);
+  // SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK);
 
-  SysTick_SetReload(RELOAD_VALUE);
+  // SysTick_SetReload(RELOAD_VALUE);
 
-  SysTick_ITConfig(ENABLE);
+  // SysTick_ITConfig(ENABLE);
 
-  SysTick_CounterCmd(SysTick_Counter_Enable);
+  // SysTick_CounterCmd(SysTick_Counter_Enable);
 }
 
 clock_time_t
@@ -56,13 +56,13 @@ clock_delay(unsigned int delay)
     unsigned int j;
     for(j = 50; j>0; j--)
     {
-      __NOP();
+      //__NOP();
     }
   }
 }
 
 void
-clock_wait(int delay)
+clock_wait(clock_time_t delay)
 {
   clock_time_t start;
   start = clock_time();
