7d7b07774603dc146cbdaf5aeefacd9367da4f2e
Bradley Cage <cage.bradley@gmail.com>
2017-12-05 21:25:45 -0800

    led functionality beginning

---
+++ arch/platform/hamilton/leds-arch.c
@@ -0,0 +1,38 @@
+#include "contiki.h"
+#include "reg.h"
+#include "dev/leds.h"
+#include "dev/gpio.h"
+
+#define LED_PIN		(19)
+
+// unsigned char leds_get(void);
+// void leds_set(unsigned char leds);
+// void leds_on(unsigned char leds);
+// void leds_off(unsigned char leds);
+// void leds_toggle(unsigned char leds);
+
+/**
+ * Leds implementation
+ */
+void leds_arch_init(void)
+{
+	REG(GPIO_DIRSET+GPIO_DIRCLR_DIRCLR);
+	REG((GPIO_DIRSET+GPIO_DIRSET_DIRSET) |= GPIO_PIN_MASK(LED_PIN));
+
+}
+unsigned char leds_arch_get(void)
+{
+	REG(GPIO_OUT+GPIO_OUT_OUT);
+}
+
+void leds_arch_set(unsigned char leds)
+{ 
+	if(leds > 0)
+	{
+		REG((GPIO_OUTSET+GPIO_OUT_OUT) |= GPIO_PIN_MASK(LED_PIN));
+	}
+	else
+	{
+		REG((GPIO_OUTSET+GPIO_OUT_OUT) &= ~GPIO_PIN_MASK(LED_PIN));
+	}
+}
--- arch/platform/hamilton/platform.c
+++ arch/platform/hamilton/platform.c
@@ -1,3 +1,30 @@
 #include "contiki.h"
 #include "dev/leds.h"
 #include "sys/platform.h"
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+void
+platform_init_stage_one(void)
+{
+
+}
+
+void
+platform_init_stage_two(void)
+{
+
+}
+
+ void
+ platform_init_stage_three(void)
+ {
+
+ }
+
+ void
+ platform_idle(void)
+ {
+ 	
+ }
