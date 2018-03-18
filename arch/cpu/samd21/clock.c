64216f468a04f7fa37087046944b490f594f634f
Billy Zhao <william_zhao@berkeley.edu>
2018-03-17 21:35:32 -0700

    Flushed out clock init file to use correct clocks.

--- arch/cpu/samd21/clock.c
+++ arch/cpu/samd21/clock.c
@@ -2,17 +2,113 @@
 #include "contiki.h"
 #include "clock.h"
 #include "headers/samr21e18a.h"
-
+#include "component/pm.h"
 static volatile unsigned long seconds;
 static volatile clock_time_t ticks;
 
-
+#define CLOCK_CORECLOCK     (32000000U)
+#define WAITSTATES          ((CLOCK_CORECLOCK - 1) / 24000000)
 #define RELOAD_VALUE		((F_CPU / CLOCK_CONF_SECOND) - 1)
+#define CLOCK_OSCULP32K      32768U
 
 void
 clock_init(void)
 {
+  //Configure clock for systick period	
   SysTick_Config(.1);
+  // enable clocks for the power, sysctrl and gclk modules 
+  PM->APBAMASK.reg = (PM_APBAMASK_PM | PM_APBAMASK_SYSCTRL | PM_APBAMASK_GCLK); 
+  PM->APBBMASK.reg |= PM_APBBMASK_NVMCTRL; 
+  NVMCTRL->CTRLB.reg |= NVMCTRL_CTRLB_RWS(WAITSTATES);
+  PM->APBBMASK.reg &= ~PM_APBBMASK_NVMCTRL; 
+  //ULP32_DFLL
+      /* reset the GCLK module so it is in a known state */
+  GCLK->CTRL.reg = GCLK_CTRL_SWRST;
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
+
+  /* Setup clock GCLK3 with divider 1 */
+  GCLK->GENDIV.reg = GCLK_GENDIV_ID(3) | GCLK_GENDIV_DIV(1);
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
+
+  /* Enable GCLK3 with OSCULP32K as source */
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(3) |
+                      GCLK_GENCTRL_GENEN |
+                      GCLK_GENCTRL_SRC_OSCULP32K;
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
+
+  /* set GCLK3 as source for DFLL */
+  GCLK->CLKCTRL.reg = (GCLK_CLKCTRL_GEN_GCLK3 |
+                       GCLK_CLKCTRL_ID_DFLL48 |
+                       GCLK_CLKCTRL_CLKEN);
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
+
+  /* Disable ONDEMAND mode while writing configurations */
+  SYSCTRL->DFLLCTRL.bit.ONDEMAND = 0;
+  while ((SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0) {
+      /* Wait for DFLL sync */
+  }
+
+  /* get the coarse and fine values stored in NVM (Section 9.3) */
+  uint32_t coarse = (*(uint32_t *)(0x806024) >> 26);  /* Bits 63:58 */
+  uint32_t fine = (*(uint32_t *)(0x806028) & 0x3FF);  /* Bits 73:64 */
+
+  SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_CSTEP(coarse >> 1) |
+                         SYSCTRL_DFLLMUL_FSTEP(fine >> 1) |
+                         SYSCTRL_DFLLMUL_MUL(CLOCK_CORECLOCK / CLOCK_OSCULP32K);
+  SYSCTRL->DFLLVAL.reg = SYSCTRL_DFLLVAL_COARSE(coarse) |
+                         SYSCTRL_DFLLVAL_FINE(fine);
+  SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_MODE;
+  while ((SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0) {
+      /* Wait for DFLL sync */
+  }
+
+  SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
+  while ((SYSCTRL->PCLKSR.reg & (SYSCTRL_PCLKSR_DFLLRDY |
+                                 SYSCTRL_PCLKSR_DFLLLCKF |
+                                 SYSCTRL_PCLKSR_DFLLLCKC)) == 0) {
+      /* Wait for DFLLLXXX sync */
+  }
+
+  /* select the DFLL as source for clock generator 0 (CPU core clock) */
+  GCLK->GENDIV.reg =  (GCLK_GENDIV_DIV(1U) | GCLK_GENDIV_ID(0));
+  GCLK->GENCTRL.reg = (GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_DFLL48M | GCLK_GENCTRL_ID(0));
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0;
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
+
+
+  /* setup generic clock 1 as 8MHz for timer.c */
+  GCLK->GENDIV.reg = (GCLK_GENDIV_DIV(CLOCK_CORECLOCK / 8000000ul) |
+                      GCLK_GENDIV_ID(1));
+  GCLK->GENCTRL.reg = (GCLK_GENCTRL_GENEN |
+                       GCLK_GENCTRL_SRC_DFLL48M |
+                       GCLK_GENCTRL_ID(1));
+
+
+
+  /* OSC8M is turned on by default and feeds GCLK0.
+   * OSC8M must be disabled after another oscillator is set 
+   * to feed GCLK0 
+   */
+  SYSCTRL->OSC8M.bit.ENABLE = 0;
+  while ((SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_OSC8MRDY)) {}
+  /* make sure we synchronize clock generator 0 before we go on */ 
+
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
+
+
+  /* Setup Clock generator 2 with divider 1 (32.768kHz) */
+  GCLK->GENDIV.reg  = (GCLK_GENDIV_ID(2)  | GCLK_GENDIV_DIV(0));
+  GCLK->GENCTRL.reg = (GCLK_GENCTRL_ID(2) | GCLK_GENCTRL_GENEN |
+          GCLK_GENCTRL_RUNSTDBY |
+          GCLK_GENCTRL_SRC_OSCULP32K);
+
+  while (GCLK->STATUS.bit.SYNCBUSY) {}
+
+  /* redirect all peripherals to a disabled clock generator (7) by default */
+  for (int i = 0x3; i <= 0x22; i++) {
+      GCLK->CLKCTRL.reg = ( GCLK_CLKCTRL_ID(i) | GCLK_CLKCTRL_GEN_GCLK7 );
+      while (GCLK->STATUS.bit.SYNCBUSY) {}
+  }
 }
 
 clock_time_t
